21b33a2e25341e8bb753df2ec10ffcc758b421f3
Benedict Hyland <benedicthyland@Mac.com>
2025-10-15 21:52:29 +0100

    Improving the name in the docker scripts for the logging directories

--- docker_forecast.sh
+++ docker_forecast.sh
@@ -5,13 +5,13 @@ set -euo pipefail
 IMAGE_NAME="hiddenbenedict/blueoctopus:v1.1.1"
 CONTAINER_NAME="forecaster"
 ENV_ROLE="forecast"
-LOCAL_DIR="/home/ec2-user/logs"
+LOCAL_LOGS="/home/ec2-user/logs"
 CONTAINER_DIR="/root"
 
-mkdir -p "$LOCAL_DIR"
+mkdir -p "$LOCAL_LOGS"
 
 docker run -it --rm \
   --name "$CONTAINER_NAME" \
-  -v "$LOCAL_DIR":"$CONTAINER_DIR" \
+  -v "$LOCAL_LOGS":"$CONTAINER_DIR" \
   -e "STARTUP_MODE"="$ENV_ROLE" \
   "$IMAGE_NAME"
--- docker_model.sh
+++ docker_model.sh
@@ -5,16 +5,16 @@ set -euo pipefail
 IMAGE_NAME="hiddenbenedict/blueoctopus:v1.1.1"
 CONTAINER_NAME="modeller"
 ENV_ROLE="model"
-LOCAL_DIR="/home/ec2-user/logs"
+LOCAL_LOGS="/home/ec2-user/logs"
 LOCAL_STATS="/home/ec2-user/data/stats"
 CONTAINER_DIR="/root"
 CONTAINER_STATS="/app/data/stats"
 
-mkdir -p "$LOCAL_DIR"
+mkdir -p "$LOCAL_LOGS"
 
 docker run -it --rm \
   --name "$CONTAINER_NAME" \
-  -v "$LOCAL_DIR":"$CONTAINER_DIR" \
+  -v "$LOCAL_LOGS":"$CONTAINER_DIR" \
   -v "$LOCAL_STATS":"$CONTAINER_STATS" \
   -e "STARTUP_MODE"="$ENV_ROLE" \
   "$IMAGE_NAME"
